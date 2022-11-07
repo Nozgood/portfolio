ef7400c8fc834f3c472b64cb8baece85a2af0c1b
Nowfeel Safi <noz@macbook-air-de-nowfeel.home>
2022-11-07 02:17:18 +0100

    initiate project

--- components/Tete.jsx
+++ components/Tete.jsx
@@ -1,10 +1,11 @@
 import Head from 'next/head'
 
 export default function Tete({ page }) {
+  const title = 'Nowfeel.dev - ' + page
   return (
     <>
       <Head>
-        <title>Nowfeel.dev - {page}</title>
+        <title> {title} </title>
         <meta
           name="description"
           content="Welcome to my fabulous Portfolio, get to know me thanks to this website !"
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,3 @@
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 import Tete from '../components/Tete'
 
 export default function Home() {
